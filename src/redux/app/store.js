b3e1b3e1f95e1161845c9a21f0d5057fdeedef00
githubscientist <sathish.datascientist@gmail.com>
2024-09-12 12:52:52 +0530

    create a new post

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import registerReducer from "../features/auth/registerSlice";
 import loginReducer from "../features/auth/loginSlice";
+import postReducer from "../features/post/postSlice";
 
 const store = configureStore({
     reducer: {
         register: registerReducer,
-        login: loginReducer
+        login: loginReducer,
+        post: postReducer,
     }
 });
 
--- src/services/postServices.js
+++ src/services/postServices.js
@@ -3,7 +3,10 @@ import instance from "./instance";
 const postServices = {
     getAllPosts: async () => {
         return await instance.get("/posts");
-    }
+    },
+    createPost: async (post) => {
+        return await instance.post("/posts", post);
+    },
 }
 
 export default postServices;
