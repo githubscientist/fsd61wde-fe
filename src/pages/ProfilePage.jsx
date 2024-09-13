8c27782e342466607d6cf1c92341ff45b44da3d6
githubscientist <sathish.datascientist@gmail.com>
2024-09-13 12:22:38 +0530

    profile picture

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,14 +1,48 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, useRevalidator } from "react-router-dom"
+import userServices from "../services/userServices";
 
 const ProfilePage = () => {
 
     const user = useLoaderData();
+    const { revalidate } = useRevalidator();
+
+    const handleUpdateProfilePicture = async (e) => {
+        // get the file
+        const file = e.target.files[0];
+
+        // create a new FormData
+        const formData = new FormData();
+
+        // append the file to FormData
+        formData.append('profilePicture', file);
+
+        userServices.updateProfilePicture(formData)
+            .then(response => {
+                alert(response.data.message);
+                revalidate();
+            })
+            .catch(error => {
+                alert(error.response.data.message);
+            });
+    }
 
     return (
         <div className="container mt-5">
             <h1>Profile</h1>
             <div className="row">
                 <div className="col-8">
+                    <div className="form-group">
+                        <img
+                            src={user.profilePicture ? 'http://localhost:3001/' + user.profilePicture : 'https://www.gravatar.com/avatar/' + user.email + '?s=200'}
+                            alt="Profile"
+                        />
+                        <input
+                            type="file"
+                            className="form-control mt-3"
+                            accept="image/*"
+                            onChange={handleUpdateProfilePicture}
+                        />
+                    </div>
                     <div className="form-group">
                         <label>Name</label>
                         <input type="text" className="form-control" value={user.name} disabled />
